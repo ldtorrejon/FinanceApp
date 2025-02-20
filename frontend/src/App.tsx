f1f11d1f2411f78635f633d7fb262cda8e03ebec
luis duran <luis.duran@maxem.energy>
2025-02-20 22:42:26 +0100

    color palette added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
 import "./App.scss";
+import FinanceDetail from "./components/FinanceDetailComponent/FinanceDetail";
 
 function App() {
-  return <div>First component</div>;
+  return (
+    <>
+      <FinanceDetail></FinanceDetail>
+      <div>Color palette:</div>
+      <ul>
+        <li style={{ color: "#BB86FC" }}>Primary Color</li>
+        <li style={{ color: "#03DAC6" }}>Secondary color</li>
+        <li style={{ color: "#E4E4E4" }}>Text color</li>
+        <li style={{ color: "#6200EE" }}>Button color</li>
+        <li style={{ color: "#03A9F4" }}>Link Color</li>
+        <li style={{ color: "#1E1E1E" }}>Background Color Dark Gray</li>
+        <li style={{ color: "#333333" }}>Background Color Light Gray</li>
+        <li style={{ color: "#4CAF50" }}>Success</li>
+        <li style={{ color: "#D32F2F" }}>Error</li>
+        <li style={{ color: "#FF9800" }}>Warning</li>
+      </ul>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/FinanceDetailComponent/FinanceDetail.tsx
@@ -0,0 +1,12 @@
+import "../../assets/FinanceDetailStyles.scss";
+
+export default function FinanceDetail() {
+  return (
+    <>
+      <div className="container">
+        <div>Finance detail component title</div>
+        <div>Finance detail component info</div>
+      </div>
+    </>
+  );
+}
