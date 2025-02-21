bdb2484b989c3d5f11c771c16253ce0255f4b44e
luis duran <alvarodurantorrejon@gmail.com>
2025-02-21 15:01:34 +0100

    (styling)mobile navbar

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,6 +1,22 @@
 import React from "react";
 import styles from "../../assets/Navbar.module.scss";
+import { Link } from "react-router";
+import piggy from "../../assets/icons/piggy.svg";
+import home from "../../assets/icons/home.svg";
+import profile from "../../assets/icons/profile.svg";
 
 export const Navbar: React.FC = () => {
-  return <div className={styles.container}>test</div>;
+  return (
+    <div className={styles.container}>
+      <Link to="">
+        <img src={piggy} />
+      </Link>
+      <Link to="">
+        <img src={home} />
+      </Link>
+      <Link to="">
+        <img src={profile} />
+      </Link>
+    </div>
+  );
 };
